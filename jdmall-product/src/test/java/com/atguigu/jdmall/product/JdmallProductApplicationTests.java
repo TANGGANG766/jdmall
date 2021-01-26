d7cf044f8672fe6159b983d25fba3b5b8297f247
asmsg <tanggangdan@gmail.com>
2021-01-26 22:31:00 +0800

    项目初始结构创建

--- jdmall-product/src/test/java/com/atguigu/jdmall/product/JdmallProductApplicationTests.java
+++ jdmall-product/src/test/java/com/atguigu/jdmall/product/JdmallProductApplicationTests.java
@@ -1,13 +1,31 @@
 package com.atguigu.jdmall.product;
 
+import com.atguigu.jdmall.product.entity.BrandEntity;
+import com.atguigu.jdmall.product.service.BrandService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class JdmallProductApplicationTests {
 
+
+    @Autowired
+    BrandService brandService;
+
     @Test
     void contextLoads() {
+
+
+        BrandEntity brandEntity = new BrandEntity();
+//        brandEntity.setName("三星");
+//        brandEntity.setDescript("机皇");
+//        brandService.save(brandEntity);
+//        System.out.println("保存成功");
+        brandEntity.setBrandId(1L);
+        brandEntity.setDescript("苹果");
+        brandEntity.setName("iphone");
+        brandService.updateById(brandEntity);
     }
 
 }
